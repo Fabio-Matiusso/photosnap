80b61d523950c0207c968f2f8977d91adc6248e2
Fábio Del Col Matiusso <fabiodclm@gmail.com>
2022-10-17 17:59:47 -0300

    Finalizando cards de planos

--- src/app/shared/components/prices-components/card-prices/card-prices.component.ts
+++ src/app/shared/components/prices-components/card-prices/card-prices.component.ts
@@ -9,6 +9,55 @@ export class CardPricesComponent implements OnInit {
   public checked: boolean = true;
   public disabled: boolean = false;
 
+  public prices: {
+    yearly: {
+      basic: string;
+      pro: string;
+      business: string;
+    };
+  } = {
+    yearly: {
+      basic: '$ 190.00',
+      pro: '$ 390.00',
+      business: '$990.00',
+    },
+  };
+
+  public plansAndDescription: {
+    basic: {
+      plan: string;
+      description: string;
+    };
+    pro: {
+      plan: string;
+      description: string;
+    };
+    business: {
+      plan: string;
+      description: string;
+    };
+  } = {
+    basic: {
+      plan: 'Basic',
+      description:
+        'Includes basic usage of our platform. Recommended for new and aspiring photographers.',
+    },
+    pro: {
+      plan: 'Pro',
+      description:
+        'More advanced features available. Recommended for photography veterans and professionals.',
+    },
+    business: {
+      plan: 'Business',
+      description:
+        'Additional features available such as more detailed metrics. Recommended for business owners.',
+    },
+  };
+
+  public ctaButtonText: string = 'PICK PLAN';
+
+  public timing: string = 'Per Year';
+
   constructor() {}
 
   ngOnInit(): void {}
@@ -16,10 +65,16 @@ export class CardPricesComponent implements OnInit {
   public changeContent() {
     this.checked = !this.checked;
 
-    if (this.checked) {
-      console.log('conteudo mudou pq é true');
+    if (!this.checked) {
+      this.timing = 'Per Month';
+      this.prices.yearly.basic = '$ 19.00';
+      this.prices.yearly.pro = '$ 39.00';
+      this.prices.yearly.business = '$ 99.00';
     } else {
-      console.log('Conteudo mudou pq é falso');
+      this.timing = 'Per Year';
+      this.prices.yearly.basic = '$ 190.00';
+      this.prices.yearly.pro = '$ 390.00';
+      this.prices.yearly.business = '$ 990.00';
     }
   }
 }
