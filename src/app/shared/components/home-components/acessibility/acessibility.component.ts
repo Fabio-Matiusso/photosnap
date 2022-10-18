323c3de52afb44b983f410837b8cefae279d846e
Fábio Del Col Matiusso <fabiodclm@gmail.com>
2022-10-18 10:06:38 -0300

    corrigindo erro de digitacao

--- src/app/shared/components/home-components/acessibility/acessibility.component.ts
+++ src/app/shared/components/home-components/acessibility/acessibility.component.ts
@@ -13,9 +13,9 @@ export class AcessibilityComponent implements OnInit {
     srcArrow: string;
   }> = [
     {
-      title: 'Create and share your photo stories. ',
+      title: 'Designed for Everyone ',
       description:
-        'Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.',
+        'Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it. ',
       cta: 'GET AN INVITE',
       srcArrow: '../../../assets/shared/desktop/arrow.svg',
     },
